90454568a51226279430ac1c60ce9f9e87f09fae
Sameep Shah <sameeppshah@hotmail.com>
2023-05-31 22:07:10 +0530

    Authentication Web API Initial Commit

---
+++ AuthenticationWebApi/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using AuthenticationManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationManager.Models;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+        public AccountController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;   
+        }
+
+        /// <summary>
+        /// Authentication
+        /// </summary>
+        /// <param name="authenticateRequest"></param>
+        /// <returns></returns>
+        [Route("authenticate")]
+        [HttpPost]
+        public ActionResult<AuthenticationResponse?> Authenticate(AuthenticationRequest authenticateRequest) 
+        {
+            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(authenticateRequest);
+            if (authenticationResponse == null)
+                return Unauthorized();
+            return Ok(authenticationResponse);
+        }
+
+    }
+}
